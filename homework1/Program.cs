ac0526dba07179176722db4d0b934acf542e23aa
Ruslan <botyrovra@gmail.com>
2022-04-01 00:12:25 +0300

    add homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(100, 1000);
+    }
+    return answer;
+}
+
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(10);
+PrintArray(arr);
+
+int count = 0;
+for (int i = 0; i<arr.Length; i++)
+{
+    if(arr[i] %2 ==0 ) count++;
+}
+Console.WriteLine($"чётных чисел в массиве {count}");
