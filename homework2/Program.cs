a86e1ae68acca11ad8047f9fa6d9275fb4a45d84
Ruslan <botyrovra@gmail.com>
2022-04-01 00:12:36 +0300

    add homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(1, 10);
+    }
+    return answer;
+}
+
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(11);
+PrintArray(arr);
+
+int sumOddPos = 0;
+int i = 1;
+while (i<arr.Length)
+{
+    sumOddPos = sumOddPos + arr[i]; i=i+2;
+}
+Console.WriteLine($"сумма элементов, стоящих на нечетных позициях = {sumOddPos}");
